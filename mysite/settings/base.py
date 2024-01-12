70852f1d9b97f6508d6a207d296f9db7a174636e
Isaacn123 <nsambai72@gmail.com>
2024-01-12 16:06:30 +0300

    change to Sqlite3

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASESs = {
+DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
@@ -113,7 +113,7 @@ DATABASESs = {
 #     }
 # } 
 
-DATABASES = {
+DATABASESs = {
     # 'default': {
     #     'ENGINE': 'django.db.backends.mysql',
     #     'NAME': 'yeshualife_db',
