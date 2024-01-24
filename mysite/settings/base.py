d3e732fe3029006ba4c89c32aa43f0e7741940a1
Isaacn123 <nsambai72@gmail.com>
2024-01-24 12:38:56 +0300

    content

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -100,7 +100,7 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASESsd = {
+DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
@@ -118,7 +118,7 @@ DATABASESsd = {
 #     }
 # } 
 
-DATABASESss= {
+DATABASESs= {
     # 'default': {
     #     'ENGINE': 'django.db.backends.mysql',
     #     'NAME': 'yeshualife_db',
@@ -142,7 +142,7 @@ DATABASESss= {
     }
 } 
 
-DATABASES = {
+DATABASESsd = {
         'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'yeshualife_db',
