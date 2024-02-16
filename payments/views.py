d57565710cb44e1653da03159cbd4fece13b9326
Isaacn123 <nsambai72@gmail.com>
2024-02-16 23:45:44 +0300

    generate payment api key

--- payments/views.py
+++ payments/views.py
@@ -36,16 +36,18 @@ def apiuser(request,):
 
 def generate_api_key(request):
    headers = {
-    # Request headers
+    # Request headers\
+    'X-Reference-Id': reference_id,
     'Content-Type': 'application/json',
     'Ocp-Apim-Subscription-Key': 'a247ad4411304f3bb624580ffe0922b9'}
    
    calback_url = 'http://myapp.com/momoapi/callback'
-   params = urllib.urlencode({ })
+   params = urllib.parse.urlencode({})
    body = json.dumps({"providerCallbackHost": calback_url})
+
    try:
      conn = httplib2.HTTPSConnection('ericssonbasicapi2.azure-api.net')
-     conn.request("POST", "/v1_0/apiuser/${reference_id}/apikey?%s" % params, body, headers)
+     conn.request("POST", f"/v1_0/apiuser/{reference_id}/apikey?%s" % params, body, headers)
      response = conn.getresponse()
      print(response.status)
      print(response.reason)
