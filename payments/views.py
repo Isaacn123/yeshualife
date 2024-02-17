9ed1155f14e64042395bd46a4050d0cbdd013918
Isaacn123 <nsambai72@gmail.com>
2024-02-17 18:03:00 +0300

    generate payment api TOKEN

--- payments/views.py
+++ payments/views.py
@@ -14,6 +14,28 @@ reference_id = str(uuid.uuid4())
 class GetTheTokenAPIView(APIView):
 
     def post(self, request):
+        api_token = self.generate_api_token()
+
+
+        if api_token:
+            access_token = self.extract_api_token_key(api_token)
+
+            return access_token
+    
+    def extract_api_token_key(self, response_data):
+        try:
+            response_json = json.loads(response_data)
+            access_token = response_json.get('access_token')
+            if access_token:
+                return access_token
+            else:
+                return HttpResponse("No Access Token Found")
+            
+        except json.JSONDecodeError as e:
+            return HttpResponse({'error': str(e)}, status=status.HTTP_406_NOT_ACCEPTABLE)
+
+    def generate_api_token(self,request):
+
         if request.method == "POST":
             payload = {}
             headers = {
@@ -36,7 +58,7 @@ class GetTheTokenAPIView(APIView):
                 conn.close()
 
                 # Return response
-                return HttpResponse(data.access_token, status=status.HTTP_200_OK)
+                return HttpResponse(data, status=status.HTTP_200_OK)
             
                 # response = requests.request("POST", url, headers=headers, data=payload)
                 # print(response.text)
@@ -46,7 +68,52 @@ class GetTheTokenAPIView(APIView):
         else:
             # Handle non-POST requests
             return HttpResponse({"error": "Method not allowed"}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+    def make_request_to_pay(self,api_token):
+        pass
 
+
+
+class RequestToPayAPIView(APIView):
+    def post(self,request):
+
+        api_access_token = GetTheTokenAPIView
+
+        if request.method =="POST":
+                conn = http.client.HTTPSConnection("sandbox.momodeveloper.mtn.com")
+                payload = json.dumps({
+                "amount": "1.0",
+                "currency": "EUR",
+                "externalId": "09898687",
+                "payer": {
+                    "partyIdType": "MSISDN",
+                    "partyId": "0700186921"
+                },
+                "payerMessage": "Helli am testing here",
+                "payeeNote": "Thank you for your Generousty"
+                })
+
+                headers = {
+                'Ocp-Apim-Subscription-Key': 'd7d2a50561a34050977f2a5504cadc49',
+                'X-Reference-Id': '169c8982-99e1-4799-be51-d8c98881cc8e',
+                'X-Target-Environment': 'sandbox',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSMjU2In0.eyJjbGllbnRJZCI6ImFkOTcyMjI1LTAwM2EtNGJmYi05MzJlLTE3OWI1ZDQ2MWQyNCIsImV4cGlyZXMiOiIyMDI0LTAyLTE3VDAwOjAzOjA5LjgyNSIsInNlc3Npb25JZCI6IjhkY2Q1MDE3LTZmYzYtNGJiZS04ZDMyLTZkMmZmOTNkZmJjZiJ9.Qq1rW1KHcC2Sy0m8ZFUqG9xlNiVrjCdpn1yfEzm2S_MHz3Vru_8r0PuycIKhbzNEB-aBmvQ_uovIwPsfM6Z6KTUH6r-Iov_9408dw99UjsR8nvI7aPvw1Fi1UuxQSkU3L_06rXxRolpdydZD_OkciYzs35IWmBi68vav7IjDtgmmqJ9VGgRz3CdL75KC5rHD5IOxOdNzDxJSyB6DxHx1ZA4B611Bo1UbXa2r6V7YspTv5278vMCMgVskeqcfjoDbutMkhChKAgt5Fi2Mq8TU3QH72x40v53D7f79rpR4FnCcqIJYufzRkh3Kh_0eBf3wWJDp9KgwVo4aIDM_COhOZA'
+                }
+
+                try:
+
+                    conn.request("POST", "/collection/v1_0/requesttopay", payload, headers)
+                    res = conn.getresponse()
+                    data = res.read()
+                    print(data.decode("utf-8"))
+
+                    return HttpResponse({"data": data}, status=status.HTTP_201_CREATED)
+                
+                except Exception as e:
+                    return HttpResponse({"error":"Internal Server Error"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR);
+        else:
+            return HttpResponse({"error":"Method not allowed!"}, status= status.HTTP_405_METHOD_NOT_ALLOWED)
+        
 def apiuser(request,):
     # reference_id = str(uuid.uuid4())
     print(reference_id)
