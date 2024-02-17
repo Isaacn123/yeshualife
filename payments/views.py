7b5cc0d4ef321f0287d3866f3501f15b927ac319
Isaacn123 <nsambai72@gmail.com>
2024-02-17 13:43:18 +0300

    Revert "generate payment api key"
    This reverts commit d57565710cb44e1653da03159cbd4fece13b9326.
    
    	modified:   payments/views.py
      new changes

--- payments/views.py
+++ payments/views.py
@@ -53,18 +53,16 @@ def generate_token_task(request):
 
 def generate_api_key(request):
    headers = {
-    # Request headers\
-    'X-Reference-Id': reference_id,
+    # Request headers
     'Content-Type': 'application/json',
     'Ocp-Apim-Subscription-Key': 'a247ad4411304f3bb624580ffe0922b9'}
    
    calback_url = 'http://myapp.com/momoapi/callback'
-   params = urllib.parse.urlencode({})
+   params = urllib.urlencode({ })
    body = json.dumps({"providerCallbackHost": calback_url})
-
    try:
      conn = httplib2.HTTPSConnection('ericssonbasicapi2.azure-api.net')
-     conn.request("POST", f"/v1_0/apiuser/{reference_id}/apikey?%s" % params, body, headers)
+     conn.request("POST", "/v1_0/apiuser/${reference_id}/apikey?%s" % params, body, headers)
      response = conn.getresponse()
      print(response.status)
      print(response.reason)
