f662a9f6bad33a1682adafb916cc67383443cb86
Isaacn123 <nsambai72@gmail.com>
2024-02-17 14:27:31 +0300

    generate payment api TOKEN

--- payments/urls.py
+++ payments/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import generate_api_key, generate_token_task 
 
 urlpatterns = [
-    path('generate-api-key/', generate_api_key, name='generate_api_key'),
-    path('trigger-token-task/', generate_token_task, name='trigger_token_task'),
+    # path('generate-api-key/', generate_api_key, name='generate_api_key'),
+    path('generate-api-token/', generate_token_task, name='generate_api_token')
 
 ]
