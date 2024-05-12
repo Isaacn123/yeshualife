8754d8cb6a147f13e886e2b82df306b8c51514da
Isaacn123 <nsambai72@gmail.com>
2024-05-12 12:14:52 +0300

    adding rss headers links

---
+++ mysite/templatetags/page_tags.py
@@ -0,0 +1,18 @@
+from django import template
+from blog.models import BlogPage
+
+register = template.Library()
+
+@register.simple_tag()
+def get_page_posts(page_id,default_image_url=None):
+    try:
+        page = BlogPage.objects.get(id=page_id)
+        image_url = page.image.url if page.image else default_image_url
+        return {
+            'title':page.title,
+            'description':page.body,
+            'image_url': image_url
+
+        }
+    except BlogPage.DoesNotExist:
+        return None
