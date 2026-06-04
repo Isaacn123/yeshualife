a49482797401d1ac570997cd4ff2acb72c5dbecd
usaac <nsambai72@gmail.com>
2026-06-04 14:39:58 +0300

    changing DB

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -139,7 +139,7 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASESn = {
+DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
@@ -157,7 +157,7 @@ DATABASESn = {
 #     }
 # } 
 
-DATABASES = {
+DATABASESx = {
     # 'default': {
     #     'ENGINE': 'django.db.backends.mysql',
     #     'NAME': 'yeshualife_db',
