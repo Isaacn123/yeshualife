b9cc97868c4d98927661a8616f7bea918a78113d
usaac <nsambai72@gmail.com>
2026-08-20 11:46:12 +0300

    correcting sliders

--- global_solutions/static/global_solutions/similar_clips.js
+++ global_solutions/static/global_solutions/similar_clips.js
@@ -37,10 +37,20 @@
     });
   }
 
+  function bindControl(el, instance, handler) {
+    el.addEventListener("click", function (event) {
+      event.preventDefault();
+      event.stopPropagation();
+      pauseAllVideos(instance._element);
+      handler();
+    });
+  }
+
   function initCarousel(el) {
     if (typeof bootstrap === "undefined" || !bootstrap.Carousel) {
       return null;
     }
+
     var instance = bootstrap.Carousel.getOrCreateInstance(el, {
       interval: false,
       ride: false,
@@ -49,14 +59,32 @@
       keyboard: true,
     });
 
-    el.addEventListener("slide.bs.carousel", function () {
-      pauseAllVideos(el);
+    var prevBtn = el.querySelector(".fh-similar-clips__control.carousel-control-prev");
+    var nextBtn = el.querySelector(".fh-similar-clips__control.carousel-control-next");
+
+    if (prevBtn) {
+      bindControl(prevBtn, instance, function () {
+        instance.prev();
+      });
+    }
+
+    if (nextBtn) {
+      bindControl(nextBtn, instance, function () {
+        instance.next();
+      });
+    }
+
+    el.querySelectorAll(".fh-similar-clips__indicators [data-bs-slide-to]").forEach(function (btn) {
+      bindControl(btn, instance, function () {
+        var index = parseInt(btn.getAttribute("data-bs-slide-to"), 10);
+        if (!isNaN(index)) {
+          instance.to(index);
+        }
+      });
     });
 
-    el.querySelectorAll(".fh-similar-clips__control, .carousel-indicators button").forEach(function (btn) {
-      btn.addEventListener("click", function () {
-        pauseAllVideos(el);
-      });
+    el.addEventListener("slide.bs.carousel", function () {
+      pauseAllVideos(el);
     });
 
     return instance;
